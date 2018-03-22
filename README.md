8d686ae09ee7687bfe61159128ca1237eddd7dd4
guojunfu <guojunfu@ledongli.me>
2018-03-22 16:34:12 +0800

    [mod] 微信分享: 补充README

--- README.md
+++ README.md
@@ -15,29 +15,51 @@
 
 ### 方法说明
 
-##### WechatHelper
+##### 微信授权
 
-* 微信授权: `WechatHelper.getInstance().authorizeByWechat()` 
+`WechatHelper.getInstance().authorizeByWechat()` 
 
-* 微信分享:
+##### 微信分享
 
-  * 对话分享: `WechatHelper.getInstance().shareWechat(ShareParams params)`
+1. 对话分享接口: 
 
-  * 朋友圈分享: `WechatHelper.getInstance().shareWechatMoments(ShareParams params)`
+  `WechatHelper.getInstance().shareWechat(ShareParams params)`
+  
+   使用参考：
+    
+   | 分享类型 | 具体方法 | 
+   | ------------- |-------------|
+   链接分享 |  WechatShareTest.shareLinkToWechat
+   网络图片分享|  WechatShareTest.shareNetworkImageToWechat
+   本地图片分享|  WechatShareTest.shareLocalImageToWechat
+   
+2. 朋友圈分享接口: 
+  
+  `WechatHelper.getInstance().shareWechatMoments(ShareParams params)`
+  
+   使用参考：
+   
+   | 分享类型 | 具体方法 | 
+   | ------------- |-------------|
+   链接分享 |  WechatShareTest.shareLinkToWechatMoments
+   网络图片分享|  WechatShareTest.shareNetworkImageToWechatMoments
+   本地图片分享|  WechatShareTest.shareLocalImageToWechatMoments
 
-##### ShareParams
+3. 分享参数ShareParams
 
-| ShareParams属性 | 功能 | 取值 | 
-| ------------- |-------------|-------------|
-mShareType | 分享类型 | `ShareType.LOCAL_IMAGE`(本地图片分享)，`ShareType.NETWORK_IMAGE`(网络图片分享)，`ShareType.LINK`(链接分享)
-mTitle | 分享标题 | String类型
-mText | 分享内容| String类型
-mLinkUrl | 分享链接| String类型
-mNetworkImageUrl | 分享网络图片链接| String类型
-mLocalBitmap | 分享本地图片| Bitmap类型
+   | ShareParams属性 | 功能 | 取值 | 
+   | ------------- |-------------|-------------|
+   mShareType | 分享类型 | `ShareType.LOCAL_IMAGE`(本地图片分享)，`ShareType.NETWORK_IMAGE`(网络图片分享)，`ShareType.LINK`(链接分享)
+   mTitle | 分享标题 | String类型
+   mText | 分享内容| String类型
+   mLinkUrl | 分享链接| String类型
+   mNetworkImageUrl | 分享网络图片链接| String类型
+  mLocalBitmap | 分享本地图片| Bitmap类型
 
 ### 备注
 
 Demo中未设置正确AppID／AppSecret，直接运行是没有效果的。
 
 欢迎关注小可爱的简书[Jimmy_gjf ](https://www.jianshu.com/u/02bba7269ac1)❤️❤️
+
+
--- app/src/main/java/com/gjf/wc/activity/WechatShareTest.java
+++ app/src/main/java/com/gjf/wc/activity/WechatShareTest.java
@@ -1,30 +1,23 @@
 package com.gjf.wc.activity;
 
 import android.graphics.BitmapFactory;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import com.gjf.wc.R;
 import com.gjf.wc.share.ShareParams;
 import com.gjf.wc.share.ShareType;
+import com.gjf.wc.utils.GlobalConfig;
 import com.gjf.wc.utils.SucceedAndFailedHandler;
 import com.gjf.wc.utils.WechatHelper;
 
-public class WechatShareActivity extends AppCompatActivity {
+public class WechatShareTest {
 
-    private final String TAG = "WechatShareActivity";
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_wechat_share);
-    }
+    private static final String TAG = "WechatShareActivity";
 
     /**
      * 微信对话  链接分享
      */
-    private void shareLinkToWechat() {
+    public static void shareLinkToWechat() {
         ShareParams params = new ShareParams();
         params.setTitle("title");
         params.setLinkUrl("https://github.com/flora-gjf/WeChatHelper");
@@ -36,7 +29,7 @@ public class WechatShareActivity extends AppCompatActivity {
     /**
      * 微信对话  网络图片分享
      */
-    private void shareNetworkImageToWechat() {
+    public static void shareNetworkImageToWechat() {
         ShareParams params = new ShareParams();
         params.setTitle("title");
         params.setNetworkImage("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1521640773430&di=726ad8d9bd244783e9c2ef97e01cab8f&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fimgad%2Fpic%2Fitem%2F18d8bc3eb13533fa02488324a2d3fd1f41345b39.jpg");
@@ -48,10 +41,11 @@ public class WechatShareActivity extends AppCompatActivity {
     /**
      * 微信对话  本地图片分享
      */
-    private void shareLocalImageToWechat() {
+    public static void shareLocalImageToWechat() {
         ShareParams params = new ShareParams();
         params.setTitle("title");
-        params.setLocalBitmap(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher));
+        params.setLocalBitmap(BitmapFactory
+                .decodeResource(GlobalConfig.getAppContext().getResources(), R.mipmap.ic_launcher));
         params.setShareType(ShareType.LOCAL_IMAGE);
 
         shareToWechat(params);
@@ -60,7 +54,7 @@ public class WechatShareActivity extends AppCompatActivity {
     /**
      * 微信朋友圈  链接分享
      */
-    private void shareLinkToWechatMoments() {
+    public static void shareLinkToWechatMoments() {
         ShareParams params = new ShareParams();
         params.setTitle("title");
         params.setLinkUrl("https://github.com/flora-gjf/WeChatHelper");
@@ -72,7 +66,7 @@ public class WechatShareActivity extends AppCompatActivity {
     /**
      * 微信朋友圈  网络图片分享
      */
-    private void shareNetworkImageToWechatMoments() {
+    public static void shareNetworkImageToWechatMoments() {
         ShareParams params = new ShareParams();
         params.setTitle("title");
         params.setNetworkImage("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1521640773430&di=726ad8d9bd244783e9c2ef97e01cab8f&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fimgad%2Fpic%2Fitem%2F18d8bc3eb13533fa02488324a2d3fd1f41345b39.jpg");
@@ -84,16 +78,17 @@ public class WechatShareActivity extends AppCompatActivity {
     /**
      * 微信朋友圈  本地图片分享
      */
-    private void shareLocalImageToWechatMoments() {
+    public static void shareLocalImageToWechatMoments() {
         ShareParams params = new ShareParams();
         params.setTitle("title");
-        params.setLocalBitmap(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher));
+        params.setLocalBitmap(BitmapFactory
+                .decodeResource(GlobalConfig.getAppContext().getResources(), R.mipmap.ic_launcher));
         params.setShareType(ShareType.LOCAL_IMAGE);
 
         shareToWechatMoments(params);
     }
 
-    private void shareToWechatMoments(ShareParams shareParams) {
+    public static void shareToWechatMoments(ShareParams shareParams) {
         WechatHelper.getInstance().shareWechatMoments(shareParams, new SucceedAndFailedHandler() {
             @Override
             public void onSuccess(Object obj) {
@@ -107,7 +102,7 @@ public class WechatShareActivity extends AppCompatActivity {
         });
     }
 
-    private void shareToWechat(ShareParams shareParams) {
+    public static void shareToWechat(ShareParams shareParams) {
         WechatHelper.getInstance().shareWechat(shareParams, new SucceedAndFailedHandler() {
             @Override
             public void onSuccess(Object obj) {
